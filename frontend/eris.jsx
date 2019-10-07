06f196aa81af77b3a51e49e9ee90d44ce9af0d59
Destiny Rose <destinyrose2358@gmail.com>
2019-10-07 18:57:43 -0500

    Adds the store to the entry file

--- frontend/eris.jsx
+++ frontend/eris.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
   let root = document.getElementById("root");
-  let store;
+  let store = configureStore();
+  window.store = store;
   ReactDOM.render(<h1>Welcome</h1>, root);
 });
