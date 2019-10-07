e1ccbc980c86c10c4a8c69ba302eb56af3bb8ee1
Destiny Rose <destinyrose2358@gmail.com>
2019-10-07 18:40:05 -0500

    Corrects issues with params

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::SessionsController < ApplicationController
 
   def create
-    @user = User.find_by_credentials(params[:users][:username], params[:users][:password])
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
     if @user
       login(@user)
       render :create
