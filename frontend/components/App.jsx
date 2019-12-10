c5f9400441706bdff8013221c163a51d67ef1de6
Destiny Rose <destinyrose2358@gmail.com>
2019-12-09 23:01:33 -0600

    Adds svg and splash page component

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,11 +4,13 @@ import SignupFormContainer from "./session/signup_form_container";
 import LoginFormContainer from "./session/login_form_container";
 import ProtectedComponents from "./protected_components";
 import UserSettingsContainer from "./user/user_settings_container";
+import SplashPage from "./splash_page/splash_page";
 
 const App = () => {
   return (
   <>
     <ProtectedComponents />
+    <Route path="/" component={ SplashPage } />
     <AuthRoute path="/signup" component={ SignupFormContainer } />
     <AuthRoute path="/login" component={ LoginFormContainer } />
     <ProtectedRoute path="/settings" component={ UserSettingsContainer} /> 
