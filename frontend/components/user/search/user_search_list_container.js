c8125b6a36b1e7c85939c60446e1cc7fff080c38
Destiny Rose <destinyrose2358@gmail.com>
2020-03-28 23:41:25 -0500

    Makes user search list filter based on username parameter

--- frontend/components/user/search/user_search_list_container.js
+++ frontend/components/user/search/user_search_list_container.js
@@ -2,12 +2,11 @@ import { connect } from "react-redux";
 import UserSearchList from "./user_search_list";
 import merge from "lodash.merge";
 
-const msp = (state, { serverId }) => {
+const msp = (state, { serverId, username }) => {
     
-    const memberIds = [...state.entities.servers[serverId].member_ids, ...state.entities.servers[serverId].pending_member_ids];
+    const memberIds = new Set([...state.entities.servers[serverId].member_ids, ...state.entities.servers[serverId].pending_member_ids]);
     const allUsers = Object.values(merge({}, state.entities.users));
-    debugger;
-    const users = allUsers.filter(user => !memberIds.includes(user.id) && user.username);
+    const users = allUsers.filter(user => !memberIds.has(user.id) && user.username.includes(username));
     
     return {
         users
