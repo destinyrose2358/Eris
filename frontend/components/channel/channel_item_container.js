27a91b64556eeb58d70e5a522a4ea8b9f0922d5f
Destiny Rose <destinyrose2358@gmail.com>
2020-04-21 23:58:48 -0500

    Debugged backend

--- frontend/components/channel/channel_item_container.js
+++ frontend/components/channel/channel_item_container.js
@@ -5,9 +5,9 @@ import ChannelItem from "./channel_item";
 const msp = (state, { channelId, match: { params: { serverId } } }) => {
   const channel = state.entities.channels[channelId];
   return {
+    members: (channel && channel.member_ids) ? channel.member_ids.map(member_id => state.entities.users[member_id] || {username: ""}) : [{username: ""}],
     serverId,
-    channel,
-    members: channel && channel.member_ids ? channel.member_ids.map(member_id => state.entities.users[member_id]) : [{username: ""}]
+    channel
   }
 };
 
